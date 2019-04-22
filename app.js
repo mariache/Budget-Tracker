d0227f3f0a262b19b5c11af37a98165fb57f3c33
SO <omarbagaev@gmail.com>
2019-04-22 23:23:54 +1200

    defined basic structure

---
+++ app.js
@@ -0,0 +1,46 @@
+
+var querySelector = document.querySelector;
+
+//Budget Controller
+var budgetController = (function () {
+
+})();
+
+//UI Controller
+var UIController = (function () {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+
+    };
+    return {
+        getInput: function () {
+            return{
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
+})();
+
+//Global App Controller
+var controller = (function (budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
+    var ctrlAddItem = function () {
+        var input = UICtrl.getInput();
+        console.log(input);
+    };
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+
+    });
+})(budgetController, UIController);
