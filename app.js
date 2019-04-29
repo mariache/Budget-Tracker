886e6aabd71f9c533efbe21d44b7f85730792377
SO <omarbagaev@gmail.com>
2019-04-29 23:11:59 +1200

    update total (inc, exp, percentage) budget controller(not for ui)

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ var budgetController = (function () {
         this.value = value
     };
 
+    var calculateTotal = function (type) {
+      var sum = 0;
+      data.allItems[type].forEach(function (curr) {
+          sum += curr.value;
+      });
+      data.totals[type] = sum;
+    };
+
     var data = {
         allItems: {
             exp: [],
@@ -24,7 +32,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
     return {
         addItem: function (type, des, val) {
@@ -44,6 +54,33 @@ var budgetController = (function () {
             data.allItems[type].push(newItem);
         return newItem;
         },
+
+        calculateBudget: function () {
+            //calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            //calculate budget
+            data.budget = data.totals.inc - data.totals.exp;
+
+            //calculate percentage
+            if (data.totals.inc > 0){
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            }else{
+                data.percentage = -1;
+            }
+
+        },
+
+        getBudget: function () {
+            return{
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
+        },
+
         testing: function () {
             console.log(data);
         }
@@ -122,7 +159,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
 
 var updateBudget = function () {
-
+    //update budget
+    budgetCtrl.calculateBudget();
+    //return budget
+    var budget = budgetCtrl.getBudget();
+    //display in UI
+    console.log(budget);
 };
 
     var ctrlAddItem = function () {
